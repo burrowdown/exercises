20a83cd1f2e80b6e70d0b1f4305b967405e98600
Danny Burrow <burrowingdan@gmail.com>
2018-03-17 14:23:31 -0700

    added values to Card

--- deck.py
+++ deck.py
@@ -4,19 +4,24 @@ from random import shuffle
 class Card:
 
     def __init__(self, face, suit):
-        self.face = face
         self.suit = suit
 
         if face == 1:
             self.face = "A"
+            # this deck is only good for games with high aces
+            self.value = 11
         elif face == 11:
             self.face = "J"
+            self.value = 10
         elif face == 12:
             self.face = "Q"
+            self.value = 10
         elif face == 13:
             self.face = "K"
+            self.value = 10
         else:
             self.face = str(face)
+            self.value = face
 
 
 class Deck:
--- test_deck.py
+++ test_deck.py
@@ -7,10 +7,14 @@ class TestCardCreation(unittest.TestCase):
     def setUp(self):
         self.king_hearts = deck.Card(13, "hearts")
         self.four_diamonds = deck.Card(4, "diamonds")
+        self.ace_spades = deck.Card(1, "spades")
 
     def test_card_creation(self):
         self.assertEqual(self.king_hearts.suit, "hearts")
         self.assertEqual(self.four_diamonds.face, "4")
+        self.assertEqual(self.ace_spades.value, 11)
+        self.assertEqual(self.king_hearts.value, 10)
+        self.assertEqual(self.four_diamonds.value, 4)
 
 
 class TestDeckCreation(unittest.TestCase):
