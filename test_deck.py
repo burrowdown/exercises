43bfef12b8f4a0869f9fa0a00e16e8f9f38c615f
Danny Burrow <burrowingdan@gmail.com>
2018-03-17 14:01:24 -0700

    card test

---
+++ test_deck.py
@@ -0,0 +1,13 @@
+import unittest
+import deck
+
+
+class TestCardCreation(unittest.TestCase):
+
+    def setUp(self):
+        self.king_hearts = deck.Card(13, "hearts")
+        self.four_diamonds = deck.Card(4, "diamonds")
+
+    def test_card_creation(self):
+        self.assertEqual(self.king_hearts.suit, "hearts")
+        self.assertEqual(self.four_diamonds.face, "four")
