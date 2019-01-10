18c6d827f23cfd8564743bf9dbcf3c492426fd3d
Danny Burrow <burrowingdan@gmail.com>
2019-01-10 13:50:37 -0800

    Add comments to server.js

--- productSearch/server.js
+++ productSearch/server.js
@@ -12,34 +12,38 @@ const server = Hapi.server({
 
 function getUri() {
   const uri_base = "http://api.walmartlabs.com/v1/items?apiKey=kjybrqfdgp3u4yv2qzcnjndj&format=json&ids=";
+  // extract ids fom CSV as string, remove line breaks
   let ids = FS.readFileSync('data_file.csv', 'utf8').replace(/\n/g, '');
   let uri = uri_base + ids;
   return uri
 };
 
 function searchProductsForKeyword(products, keyword) {
-  let output = new Array()
+  let matchingProducts = new Array()
   products.forEach((product) => {
     try {
       if (product.longDescription.toLowerCase().includes(keyword)) {
-        output.push(product.itemId);
+        // add products whose descriptions include the keyword to the array
+        matchingProducts.push(product.itemId);
       }
     }
     catch {
       // ignore products with no longDescription
     }
   })
-  return output;
+  return matchingProducts;
 };
 
 async function initialize () {
 
+  // Get product data from Walmart Product Lookup API as json
   const { res, payload } = await Wreck.get(getUri(), {json: true});
 
   server.route({
     method: 'GET',
     path: '/items/{keyword}',
     handler: (r, h) => {
+      // search all cached products for the keyword
       return searchProductsForKeyword(payload.items, r.params.keyword.toLowerCase())
     }
   });
